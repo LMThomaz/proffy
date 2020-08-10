6c8e6d04f7833016e23a8f7ff62093f1442bb67c
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-08-09 23:04:05 -0300

    Connection page TeacherList from API

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/33641830?s=400&u=a11266eb5aa74f4c05eba35d5f711206e3e622a1&v=4" alt="Leonardo Thomaz" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Leonardo Thomaz</strong>
-          <span>Doido por Café</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Estusiasta dos melhores grãos de café.
-      <br /><br />
-      Apaixonado por colher grãos e por mudar vidas de pessoas atráves do café.
-    </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-        <strong>R$ 60,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+
+        <a
+          target="_blanck"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
-        Entre em contato
-      </button>
+          Entre em contato
+        </a>
       </footer>
     </article>
   );
