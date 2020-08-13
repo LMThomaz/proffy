639bd6a7a8e69c73e8494ce8eb7977b80d93686a
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2020-08-13 07:46:45 -0300

    Styles in page GiveClasses

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +14,7 @@ function AppStack() {
         headerShown: false,
       }}>
         <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
       </Navigator>
     </NavigationContainer>
   );
